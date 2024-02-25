a7920e4cd338033fdcd6c9a400cf8f34f1610c7f
ijenims <77921755+ijenims@users.noreply.github.com>
2024-02-25 20:48:39 +0900

    Update cheese_fair.py

--- cheese_fair.py
+++ cheese_fair.py
@@ -1,8 +1,8 @@
 import streamlit as st
 # import plotly.express as px
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 # 画面設定
 # st.set_page_config(layout='wide')
